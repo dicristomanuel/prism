67c19ebb935b44203db9e580a12e1bc64fdbbc5e
dicristomanuel <manuel.dicristo@raise.com>
2016-11-21 20:36:17 -0600

    [npm] closing for npm

--- example/states/config.js
+++ example/states/config.js
@@ -1,8 +1,8 @@
 import { OnInit, OnBot, OffBot, OnMs, OffMs } from './all';
 
 const onUpdate = (data) => {
-    console.log('onUpdate >> ', data);
-    return data;
+  console.log('onUpdate >> ', data);
+  return data;
 }
 
 export default [
@@ -11,21 +11,3 @@ export default [
   { from: 'ms',   to: 'ms',   on: OnMs,   off: OffMs },
   { onUpdate },
 ];
-
-const first =  [ 'init', 'init', OnInit, null ];
-const second = [ 'bot', 'bot', OnBot, OffBot ];
-const third =  [ 'ms', 'ms', OnMs, OffMs ];
-
-const buildStates = (args) => {
-  console.log(args.length);
-  let states = [];
-
-  for(let i = 0; i <= 3; i++) {
-    let state = {
-
-    };
-    states.push(state);
-  }
-}
-
-buildStates(first, second, third);
